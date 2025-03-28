614be7d885fa256222f24a4e33de92430c319adf
unknown <rtallarr@gmail.com>
2025-03-28 16:49:41 -0300

    feat: add script to delete build

---
+++ cleanup.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Search and delete 'dist' and 'build' folders recursively
+for /d %%d in (dist,build,FileScape.egg-info) do (
+    if exist "%%d" (
+        echo Deleting: "%%d"
+        rmdir /s /q "%%d"
+    )
+)
+
+echo Cleanup complete!
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 VERSION = '0.0.2' 
 DESCRIPTION = 'A simple tool to declutter and organize your files.'
-LONG_DESCRIPTION = 'FileTidy helps you sort and clean up your messy folders by automatically categorizing files.'
+LONG_DESCRIPTION = 'FileScape helps you sort and clean up your messy folders by automatically categorizing files.'
 
 setup(
         name="FileScape", 
@@ -13,7 +13,7 @@ setup(
         packages=find_packages(),
         url='https://github.com/rtallarr/FileScape',
         install_requires=[],
-        keywords=['files', 'sort', 'organize'],
+        keywords=['files', 'sort', 'organize', 'organise'],
         classifiers= [
             "Programming Language :: Python :: 3",
         ]
